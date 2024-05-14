6c9945eb9fd85a16564a43921b60957e17101b88
user <user@pc-235.local>
2024-05-14 11:41:33 +0300

    added apidoc and twig

--- app/src/Controller/BookCategoryController.php
+++ app/src/Controller/BookCategoryController.php
@@ -4,20 +4,28 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Model\BookCategoryListResponse;
 use App\Service\BookCategoryService;
+use Nelmio\ApiDocBundle\Annotation\Model;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use OpenApi\Attributes as OA;
 
 class BookCategoryController extends AbstractController
 {
     public function __construct(
-        private BookCategoryService $bookCategoryService
+        private readonly BookCategoryService $bookCategoryService
     )
     {
     }
 
-    #[Route('/api/v1/book/categories', name: 'apiBookCategory')]
+    #[OA\Response(
+        response: 200,
+        description: 'Return book categories',
+        content: new Model(type: BookCategoryListResponse::class)
+    )]
+    #[Route(path: '/api/v1/book/categories', name: 'apiBookCategory', methods: ['GET'])]
     final public function categories(): Response
     {
         return $this->json($this->bookCategoryService->getCategories());
